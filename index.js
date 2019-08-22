5946289f3995ce80dc03867a6440bd4a049e8db3
AlexanderMac <amatsibarov@gmail.com>
2019-08-22 12:25:37 +0300

    remove predefined errors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 'use strict';
 
 module.exports = require('./src/custom-errors');
-module.exports.predefined = require('./src/predefined');
--- src/predefined/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-exports.http = require('./http-errors');
