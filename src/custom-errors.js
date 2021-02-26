714a74037ddc084cce5d92b26e9e714384d3482e
alexmac <amatsibarov@gmail.com>
2021-02-26 12:42:27 +0300

    fix eslint warns

--- src/custom-errors.js
+++ src/custom-errors.js
@@ -1,63 +1,63 @@
-const _ = require('lodash');
-const BaseError = require('./base-error');
+const _ = require('lodash')
+const BaseError = require('./base-error')
 
-const _customErrors = [];
+const _customErrors = []
 
 exports.registerError = (name, statusCode, messageTemplate) => {
-  _validate(name);
+  _validate(name)
 
-  name = _.upperFirst(name);
-  let CustomError = _createCustomError(name, statusCode);
+  name = _.upperFirst(name)
+  let CustomError = _createCustomError(name, statusCode)
 
-  _createFunctions(name, CustomError, messageTemplate);
+  _createFunctions(name, CustomError, messageTemplate)
 
   _customErrors.push({
     name,
     type: CustomError
-  });
-};
+  })
+}
 
 exports.isKnownError = (err) => {
-  return _.some(_customErrors, custErr => err instanceof custErr.type || err === custErr.name);
-};
+  return _.some(_customErrors, custErr => err instanceof custErr.type || err === custErr.name)
+}
 
 function _validate(name) {
   if (!name) {
-    throw new Error('Custom error name is required');
+    throw new Error('Custom error name is required')
   }
   if (_.some(_customErrors, { name })) {
-    throw new Error(`Custom error ${name} is already registered`);
+    throw new Error(`Custom error ${name} is already registered`)
   }
 }
 
 function _createCustomError(name, statusCode) {
   class CustomError extends BaseError {
     constructor(message, details) {
-      super(message, statusCode, details);
-      this.name = name + 'Error';
+      super(message, statusCode, details)
+      this.name = name + 'Error'
     }
   }
-  return CustomError;
+  return CustomError
 }
 
 function _createFunctions(name, CustomError, messageTemplate) {
-  exports[`is${name}Error`] = (err) => err instanceof CustomError;
+  exports[`is${name}Error`] = (err) => err instanceof CustomError
 
-  exports[`get${name}ErrorType`] = () => CustomError;
+  exports[`get${name}ErrorType`] = () => CustomError
 
   exports[`get${name}Error`] = (msg) => {
     if (messageTemplate && !_.isString(msg) && _.isObject(msg)) {
-      let msgParams = msg;
+      let msgParams = msg
       msg = _.reduce(msgParams, (result, paramVal, paramKey) => {
-        result = _.replace(result, '${' + paramKey + '}', paramVal);
-        return result;
-      }, messageTemplate);
+        result = _.replace(result, '${' + paramKey + '}', paramVal)
+        return result
+      }, messageTemplate)
     }
-    return new CustomError(msg);
-  };
+    return new CustomError(msg)
+  }
 
   exports[`throw${name}Error`] = (msg) => {
-    let err = exports[`get${name}Error`](msg);
-    throw err;
-  };
+    let err = exports[`get${name}Error`](msg)
+    throw err
+  }
 }
--- test/custom-errors-spec.js
+++ test/custom-errors-spec.js
@@ -1,147 +1,147 @@
-const should = require('should');
-const rewire = require('rewire');
-const customErrors = rewire('../src/custom-errors');
+const should = require('should')
+const rewire = require('rewire')
+const customErrors = rewire('../src/custom-errors')
 
-should.config.checkProtoEql = false;
+should.config.checkProtoEql = false
 
 /* eslint new-cap: off */
 describe('errors', () => {
   beforeEach(() => {
-    customErrors.__set__('_customErrors', []);
-  });
+    customErrors.__set__('_customErrors', [])
+  })
 
   describe('registerError', () => {
     function test1(name, code, template, expectedErrors) {
-      customErrors.registerError(name, code, template);
+      customErrors.registerError(name, code, template)
 
-      let actualErrors = customErrors.__get__('_customErrors');
-      should(actualErrors).eql(expectedErrors);
+      let actualErrors = customErrors.__get__('_customErrors')
+      should(actualErrors).eql(expectedErrors)
 
-      let actualError = new actualErrors[0].type('Test');
-      let expectedError = new expectedErrors[0].type('Test');
-      should(actualError).eql(expectedError);
+      let actualError = new actualErrors[0].type('Test')
+      let expectedError = new expectedErrors[0].type('Test')
+      should(actualError).eql(expectedError)
     }
 
     function test2(name, code, template, expectedErrorMessage) {
-      should(customErrors.registerError.bind(null, name, code, template)).throw(expectedErrorMessage);
+      should(customErrors.registerError.bind(null, name, code, template)).throw(expectedErrorMessage)
     }
 
     it('should register a new unique customError', () => {
-      let name = 'AccessDenied';
-      let code = 403;
-      let template = '${user} has\'t access to this data';
+      let name = 'AccessDenied'
+      let code = 403
+      let template = '${user} has\'t access to this data'
       let expectedErrors = [{
         name,
         type: customErrors.__get__('_createCustomError')(name, code)
-      }];
+      }]
 
-      test1(name, code, template, expectedErrors);
-    });
+      test1(name, code, template, expectedErrors)
+    })
 
     it('should throw an error if the name is not defined', () => {
-      let name = null;
-      let code = 403;
-      let template = '${user} has\'t access to this data';
-      let expectedErrorMessage = 'Custom error name is required';
+      let name = null
+      let code = 403
+      let template = '${user} has\'t access to this data'
+      let expectedErrorMessage = 'Custom error name is required'
 
-      test2(name, code, template, expectedErrorMessage);
-    });
+      test2(name, code, template, expectedErrorMessage)
+    })
 
     it('should throw an error if the customError is already registered', () => {
-      let name = 'AccessDenied';
-      let code = 403;
-      let template = '${user} has\'t access to this data';
-      let expectedErrorMessage = `Custom error ${name} is already registered`;
+      let name = 'AccessDenied'
+      let code = 403
+      let template = '${user} has\'t access to this data'
+      let expectedErrorMessage = `Custom error ${name} is already registered`
 
-      customErrors.__set__('_customErrors', [{ name }]);
+      customErrors.__set__('_customErrors', [{ name }])
 
-      test2(name, code, template, expectedErrorMessage);
-    });
-  });
+      test2(name, code, template, expectedErrorMessage)
+    })
+  })
 
   describe('isKnownError', () => {
     beforeEach(() => {
-      customErrors.registerError('AccessDenied', 403);
-      customErrors.registerError('ObjectNotFound', 404);
-    });
+      customErrors.registerError('AccessDenied', 403)
+      customErrors.registerError('ObjectNotFound', 404)
+    })
 
     function test(err, expected) {
-      let actual = customErrors.isKnownError(err);
-      should(actual).eql(expected);
+      let actual = customErrors.isKnownError(err)
+      should(actual).eql(expected)
     }
 
     it('should return false when err isn\'t an instance of ObjectNotFoundError', () => {
-      let err = new Error('Test Error');
-      let expected = false;
+      let err = new Error('Test Error')
+      let expected = false
 
-      test(err, expected);
-    });
+      test(err, expected)
+    })
 
     it('should return true when err is an instance of ObjectNotFoundError', () => {
-      let err = customErrors.getObjectNotFoundError('Test Error');
-      let expected = true;
+      let err = customErrors.getObjectNotFoundError('Test Error')
+      let expected = true
 
-      test(err, expected);
-    });
+      test(err, expected)
+    })
 
     it('should return true when err is a name of ObjectNotFoundError type', () => {
-      let err = 'ObjectNotFound';
-      let expected = true;
+      let err = 'ObjectNotFound'
+      let expected = true
 
-      test(err, expected);
-    });
-  });
+      test(err, expected)
+    })
+  })
 
   describe('Helpers', () => {
     beforeEach(() => {
-      let name = 'AccessDenied';
-      let code = 403;
-      let template = '${user} has\'t access to this data';
+      let name = 'AccessDenied'
+      let code = 403
+      let template = '${user} has\'t access to this data'
 
-      customErrors.registerError(name, code, template);
-    });
+      customErrors.registerError(name, code, template)
+    })
 
     describe('is<name>Error', () => {
       it('should return false when err is an instance of AccessDeniedError', () => {
-        let err = new Error('test');
-        let actual = customErrors.isAccessDeniedError(err);
-        let expected = false;
+        let err = new Error('test')
+        let actual = customErrors.isAccessDeniedError(err)
+        let expected = false
 
-        should(actual).equal(expected);
-      });
+        should(actual).equal(expected)
+      })
 
       it('should return true when err is an instance of AccessDeniedError', () => {
-        let err = customErrors.getAccessDeniedError('test');
-        let actual = customErrors.isAccessDeniedError(err);
-        let expected = true;
+        let err = customErrors.getAccessDeniedError('test')
+        let actual = customErrors.isAccessDeniedError(err)
+        let expected = true
 
-        should(actual).equal(expected);
-      });
-    });
+        should(actual).equal(expected)
+      })
+    })
 
     describe('get<name>ErrorType', () => {
       it('should return customError type', () => {
-        let actual = customErrors.getAccessDeniedErrorType();
-        let expected = customErrors.__get__('_customErrors')[0].type;
+        let actual = customErrors.getAccessDeniedErrorType()
+        let expected = customErrors.__get__('_customErrors')[0].type
 
-        should(actual).equal(expected);
-      });
-    });
+        should(actual).equal(expected)
+      })
+    })
 
     describe('get<name>Error', () => {
       it('should return an instance of customError type', () => {
-        let actual = customErrors.getAccessDeniedError({ user: 'user1' });
-        let expected = new (customErrors.__get__('_customErrors')[0].type)('user1 has\'t access to this data');
+        let actual = customErrors.getAccessDeniedError({ user: 'user1' })
+        let expected = new (customErrors.__get__('_customErrors')[0].type)('user1 has\'t access to this data')
 
-        should(actual).eql(expected);
-      });
-    });
+        should(actual).eql(expected)
+      })
+    })
 
     describe('throw<name>Error', () => {
       it('should throw an instance of customError type', () => {
-        let expected = new Error('user1 has\'t access to this data');
-        should(customErrors.throwAccessDeniedError.bind(customErrors, { user: 'user1' })).throw(expected.message);
-      });
-    });
-  });
-});
+        let expected = new Error('user1 has\'t access to this data')
+        should(customErrors.throwAccessDeniedError.bind(customErrors, { user: 'user1' })).throw(expected.message)
+      })
+    })
+  })
+})
